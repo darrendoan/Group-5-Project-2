f61e15e38a739628400fc0d1c7c20a98934edad1
Nat Poulson <nat.poulson@outlook.com.au>
2024-03-04 21:36:12 +1100

    Make adjustments to config and proto code to source port number via .env

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.AUTH_SECRET,
-    baseURL: `http://localhost:${PORT}`,
+    baseURL: `http://localhost:${process.env.DEV_PORT}`,
     clientID: process.env.AUTH_CLIENT,
     issuerBaseURL: process.env.AUTH_ISSUER_URL
 };
