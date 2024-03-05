0eb323d29196ceaf7af5688b93f804e35cdc391d
brxwnsugxr <jsdev.mas@gmail.com>
2024-03-05 13:39:42 +1100

    creating config sequlize connection

--- config/connection.js
+++ config/connection.js
@@ -2,3 +2,9 @@ const Sequlize = require('sequelize');
 
 require('dotenv').config();
 
+const Sequlize = new Sequlize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+)
+
