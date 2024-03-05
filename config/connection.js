ebfbe38182791d10cf958b8bcaaff668ca18b045
brxwnsugxr <jsdev.mas@gmail.com>
2024-03-05 13:43:43 +1100

    adding comments complete connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,23 @@
-const Sequlize = require('sequelize');
+// Import the Sequelize library for connecting to databases
+const Sequelize = require('sequelize');
 
+// Use "dotenv" to access environment variables from a `.env` file
 require('dotenv').config();
 
-const Sequlize = new Sequlize(
+// Create a Sequelize connection instance
+const sequelize = new Sequelize(
+    // Read database name from the environment variable DB_NAME
     process.env.DB_NAME,
+    // Read database user from the environment variable DB_USER
     process.env.DB_USER,
+    // Read database password from the environment variable DB_PASSWORD
     process.env.DB_PW,
-)
 
+  {
+    host: 'localhost', // Database host (localhost in this case)
+    dialect: 'mysql', // Database dialect (MySQL in this case)
+    port: 3306, // Database port (default MySQL port)
+  }
+);
+
+module.exports = sequelize;
