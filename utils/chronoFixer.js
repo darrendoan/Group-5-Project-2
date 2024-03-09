55300f7851f0d332247ba16bae508b439ec11c38
Nat Poulson <nat.poulson@outlook.com.au>
2024-03-09 11:53:39 +1100

    Create file for timestamp utilities and lay out framework for functions to create

---
+++ utils/chronoFixer.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Functions
+ * - inferTimezone(ip) - Return the timezone of the user relative to their requesting IP
+ * - normaliseTimestamp(timestamp) - Strip user's timezone offset for a normalised value
+ * - getRelativeTimestamp(timestamp, offset) - Return a string with the timezone represented relative to the given timezone
+ */
+
+function inferTimezone(ip) {
+
+}
+
+function normaliseTimestamp(timestamp) {
+    
+}
+
+function getRelativeTimestamp(timestamp, offset) {
+
+}
