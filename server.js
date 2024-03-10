792e31d37602429f4fd30ea2edb9f0924a275dad
brxwnsugxr <jsdev.mas@gmail.com>
2024-03-10 17:02:17 +1100

    debugging

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 // Import necessary modules
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers'); // Import routes
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers'); // Import helper functions
+//const helpers = require('./utils/helpers'); // Import helper functions
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const hbs = require('hbs');
 // Create an Express application instance
 const app = express();
 const PORT = process.env.PORT || 3001; // Set the port for the server
-const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });
 
 // Set up Handlebars as the template engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', );
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views/layout'));
 
 // Configure session middleware
 const sess = {
@@ -42,7 +45,11 @@ app.use((err, req, res, next) => {
 });
 
 // Define and use routes
-app.use(routes);
+//app.use(routes);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // Start the server after synchronizing Sequelize models with the database
 sequelize.sync({ force: false }).then(() => {
