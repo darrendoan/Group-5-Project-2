c32a2c94d9a9e0e64c772f52f8137bf6934abb3a
Nat Poulson <97604337+natpoulson@users.noreply.github.com>
2024-03-11 16:52:45 +1100

    Fix issue with baseURL attribute in Auth0 config

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ const config = {
     authRequired: false,
     auth0Logout: true,
     secret: process.env.AUTH_SECRET,
-    baseURL: `http://localhost:${process.env.DEV_PORT}`,
+    baseURL: process.env.AUTH_BASEURL,
     clientID: process.env.AUTH_CLIENT,
     issuerBaseURL: process.env.AUTH_ISSUER_URL
 };
